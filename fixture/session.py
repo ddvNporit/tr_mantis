c6b40d59637f55b2eb42709226c7f22b9bd7393e
ddv5 <dima.nporit@gmail.com>
2023-04-07 12:14:11 +0300

    поменял селекторы

--- fixture/session.py
+++ fixture/session.py
@@ -8,15 +8,17 @@ class SessionHelper():
         wd.find_element_by_name("username").click()
         wd.find_element_by_name("username").clear()
         wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_xpath(u"//input[@value='Вход']").click()
         wd.find_element_by_name("password").click()
         wd.find_element_by_name("password").clear()
         wd.find_element_by_name("password").send_keys(password)
-        wd.find_element_by_xpath("//input[@type='submit']").click()
+        wd.find_element_by_xpath(u"//input[@value='Вход']").click()
+        # wd.find_element_by_xpath("//input[@type='submit']").click()
 
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user")
+
     def ensure_logout(self):
         wd = self.app.wd
         if self.is_logged_in():
@@ -33,12 +35,15 @@ class SessionHelper():
     def is_logged_in_as(self,username):
         wd = self.app.wd
         return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "(" + username + ")"
-
-    def ensure_login(self, username, password):
+    def get_logged_user(self):
         wd = self.app.wd
-        if self.is_logged_in():
-            if self.is_logged_in_as(username):
-                return
-            else:
-                self.logout()
-        self.login(username, password)
+        return  wd.find_element_by_xpath("td.login-info-left span").text #wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
+
+    # def ensure_login(self, username, password):
+    #     wd = self.app.wd
+    #     if self.is_logged_in():
+    #         if self.is_logged_in_as(username):
+    #             return
+    #         else:
+    #             self.logout()
+    #     self.login(username, password)
