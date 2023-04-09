851ec8841d5e384004d24d1dcea107ec63e5ad25
ddv5 <dima.nporit@gmail.com>
2023-04-09 21:17:23 +0300

    add_project завершен

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -10,6 +10,9 @@ def test_add_project(app):
     old_list = app.project.get_project_list()
     testdata = Project(name=random_string(10))
     app.project.add_project(testdata)
+    new_list = app.project.get_project_list()
+    old_list.append(testdata)
+    assert sorted(old_list, key=Project.id_or_max) == sorted(new_list, key=Project.id_or_max)
 
 
 def random_string(maxlen):
