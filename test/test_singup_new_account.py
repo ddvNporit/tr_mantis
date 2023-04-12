665d09961e2087ed1bbeabbffb09665a09b0d8f1
ddv5 <dima.nporit@gmail.com>
2023-04-12 20:15:14 +0300

    lesson 9.4

--- test/test_singup_new_account.py
+++ test/test_singup_new_account.py
@@ -1,7 +1,20 @@
 # Name : test_singup_new_account.py
 # Author : "Denisov Dmitry"
 # Time : 12.04.2023
+import string, random
+
+
 def test_signup_account(app):
-    username ="user1"
+    username = random_username("user_", 10)
     password = "test"
+    email = username + "@localhost"
     app.james.ensure_user_exists(username, password)
+    app.signup.new_user(username, email, password)
+    app.session.login(username, password)
+    assert app.session.is_logged_in_as(username)
+    app.session.logout()
+
+
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
